e5d89a9f232c8f8669e889029f1d1e5b906ac8e7
ldolne <dolnelaetitia@gmail.com>
2020-09-09 12:00:19 +0200

    Test deployment2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import "./scss/app";
 
 const mountNode = document.querySelector("#app");
-ReactDOM.render(<App name={"Jane"} />, mountNode);
+ReactDOM.render(<App name={"Laëtitia"} />, mountNode);
